85ba33ad41f5d4f17e0d70a7e8aad3332b919a51
trevoraragon7 <trevoraragon7@users.noreply.github.com>
2026-06-01 11:45:12 -0600

    Update funnel redesign

---
+++ aragon-web/app/page.tsx
@@ -0,0 +1,9 @@
+import { HeroScrollDemo } from "@/components/ui/hero-scroll-demo";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white dark:bg-black">
+      <HeroScrollDemo />
+    </main>
+  );
+}
